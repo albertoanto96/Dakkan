07da1d622c57e4851218e9c910d95accafb0169a
Dulcecu <atm199090@gmail.com>
2017-05-04 17:07:14 +0200

    Delete auth.js

--- config/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    'facebookAuth':{
-        clientID:'1798132503847698',
-        clientSecret:'4a1030d3d49dbf1972b27439c4de3e51',
-        callbackURL:'http://localhost:3500/auth/facebook/callback',
-        profileFields:['id','displayName','photos','emails']
-    }
-}
