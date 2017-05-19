7b7a8a7a2045f80d7a97412495871954d12b240b
adria.marti <adria.marti@aaaida.com>
2017-05-19 11:34:53 +0200

    revpending

--- public/js/controllers/user.controller.js
+++ public/js/controllers/user.controller.js
@@ -138,8 +138,8 @@
                     userSRV.getreviews(nme, function (rev) {
                         if (rev!=="") {
                             console.log("REVIEWS");
-                            console.log(rev);
-                            $scope.reviews2.push(rev);
+                            console.log(rev[0]);
+                            $scope.reviews2.push(rev[0]);
                         }
                     });
                 }
